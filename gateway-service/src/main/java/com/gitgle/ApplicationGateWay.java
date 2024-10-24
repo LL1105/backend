c53ac4bfc0df57c5e11cf496305ad821f79fd371
liewstar <2935437378@qq.com>
2024-10-24 13:07:37 +0800

    整理框架

---
+++ gateway-service/src/main/java/com/gitgle/ApplicationGateWay.java
@@ -0,0 +1,12 @@
+package com.gitgle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationGateWay {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationGateWay.class, args);
+    }
+}
