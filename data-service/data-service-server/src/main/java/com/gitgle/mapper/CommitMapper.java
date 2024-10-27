c1ccc52911f8d367945555aded1950fa768935f3
liewstar <2935437378@qq.com>
2024-10-27 18:01:12 +0800

    删除空目录

---
+++ data-service/data-service-server/src/main/java/com/gitgle/mapper/CommitMapper.java
@@ -0,0 +1,15 @@
+package com.gitgle.mapper;
+
+import com.gitgle.dao.Commit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CommitMapper extends BaseMapper<Commit> {
+
+}
+
+
+
+
