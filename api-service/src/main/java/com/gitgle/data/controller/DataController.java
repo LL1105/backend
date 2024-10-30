e9249d86701c39e1df7800691516dcb19379f9d2
helloworld-mjj <962723947@qq.com>
2024-10-30 23:34:46 +0800

    添加获取热门领域接口

--- api-service/src/main/java/com/gitgle/data/controller/DataController.java
+++ api-service/src/main/java/com/gitgle/data/controller/DataController.java
@@ -2,9 +2,11 @@ package com.gitgle.data.controller;
 
 import com.gitgle.constant.RpcResultCode;
 import com.gitgle.response.GithubDataResponse;
+import com.gitgle.response.HotDomainResponse;
 import com.gitgle.result.R;
 import com.gitgle.result.RpcResult;
 import com.gitgle.service.GithubDataService;
+import com.gitgle.service.RpcDomainService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,9 @@ public class DataController {
     @DubboReference
     private GithubDataService githubDataService;
 
+    @DubboReference
+    private RpcDomainService rpcDomainService;
+
     @GetMapping("/all")
     public R<GithubDataResponse> getAllGithubData(){
         RpcResult<GithubDataResponse> allGithubData = githubDataService.getAllGithubData();
@@ -24,5 +29,14 @@ public class DataController {
             return R.Failed();
         }
         return R.Success(allGithubData.getData());
+    }
+
+    @GetMapping("/hot/domain")
+    public R<HotDomainResponse> getHotDomain(){
+        RpcResult<HotDomainResponse> hotDomain = rpcDomainService.getHotDomain();
+        if(!RpcResultCode.SUCCESS.equals(hotDomain.getCode())){
+            return R.Failed();
+        }
+        return R.Success(hotDomain.getData());
     }
 }
