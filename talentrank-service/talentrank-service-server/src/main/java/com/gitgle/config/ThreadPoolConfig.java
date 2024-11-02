a2f7918438b6ffe47939c26994364841a48fdce1
helloworld-mjj <962723947@qq.com>
2024-11-02 23:42:49 +0800

    添加线程池

---
+++ talentrank-service/talentrank-service-server/src/main/java/com/gitgle/config/ThreadPoolConfig.java
@@ -0,0 +1,23 @@
+package com.gitgle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor talentRankThreadPoolTask(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(16);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("talentRank-calculate-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+}
