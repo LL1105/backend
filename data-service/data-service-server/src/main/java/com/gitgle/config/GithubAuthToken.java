110ffaa1964da7aa8f2b97efaac7e998829a9fac
liewstar <2935437378@qq.com>
2024-11-04 03:39:41 +0800

    更改密钥获取方式

--- data-service/data-service-server/src/main/java/com/gitgle/config/GithubAuthToken.java
+++ data-service/data-service-server/src/main/java/com/gitgle/config/GithubAuthToken.java
@@ -1,8 +1,15 @@
 package com.gitgle.config;
 
+import com.gitgle.constant.RedisConstant;
+import io.netty.util.internal.ObjectUtil;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "github.auth-token")
@@ -10,11 +17,17 @@ public class GithubAuthToken {
 
     private List<String> list;
 
+    @Resource
+    StringRedisTemplate redisTemplate;
+
     public List<String> getList() {
         return list;
     }
 
     public void setList(List<String> list) {
+        if(ObjectUtils.isEmpty(list)) {
+            list = redisTemplate.opsForList().range(RedisConstant.GITHUB_TOKEN, 0, -1);
+        }
         this.list = list;
     }
 }
